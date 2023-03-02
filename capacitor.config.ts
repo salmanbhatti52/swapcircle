5c13c8e7ab69775ad9d79ad7242aefbbfa3a8421
moavizq5555 <57653982+Najam-khan@users.noreply.github.com>
2023-03-02 17:13:58 +0500

    commit

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
+  appId: 'com.swapcircle.app',
   appName: 'swapcircle',
   webDir: 'www',
   bundledWebRuntime: false,
