c8241007aa035dda1d8b6c90689f9b0886a6669a
moavizq5555 <57653982+Najam-khan@users.noreply.github.com>
2023-03-28 16:35:47 +0500

    commit

--- src/app/createsawap/createsawap.page.ts
+++ src/app/createsawap/createsawap.page.ts
@@ -1,5 +1,7 @@
 import { Location } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
+import { ApiService } from '../services/api.service';
+import { ExtraService } from '../services/extra.service';
 
 @Component({
   selector: 'app-createsawap',
@@ -15,13 +17,47 @@ export class CreatesawapPage implements OnInit {
   showcurr = false;
   showexccurr = false;
   currencies = [{ curr: 'Euro' }, { curr: 'Dollar' }, { curr: 'INR' }]
-  constructor(public location: Location) { }
+  walletslist: any;
+  currsymbol: any;
+  currID: any;
+  fromcurrency: any;
+  fromwalletamount: any;
+  towalletamount: any;
+  fromamount = false;
+  Toamount = false;
+  constructor(public location: Location,
+    public api: ApiService,
+    public extra: ExtraService) { }
 
   ngOnInit() {
+    this.getbasecurr()
+    this.walletlist()
   }
+
+
+
 
   goback() {
     this.location.back()
+  }
+  getbasecurr() {
+    this.api.sendRequest('get_currencies_by_id', { "system_currencies_id": localStorage.getItem('systemcurr') }).subscribe((curr: any) => {
+      console.log(curr);
+      // this.basecurrency = curr.data[0].name + '(' + curr.data[0].code + ')'
+      this.basecurrency = '(' + curr.data[0].symbol + ')' + curr.data[0].name
+      this.currID = curr.data[0].system_currencies_id
+      this.currsymbol = curr.data[0].symbol
+
+    })
+  }
+  walletlist() {
+    let datasend = {
+      "users_customers_id": localStorage.getItem('user_id'),
+    }
+    this.api.sendRequest('get_wallet', datasend).subscribe((response: any) => {
+      console.log('get_wallet=========', response);
+      this.walletslist = response.data
+    })
   }
 
   openList() {
@@ -29,6 +65,7 @@ export class CreatesawapPage implements OnInit {
       this.showcurr = false;
     } else {
       this.showcurr = true;
+      this.fromamount = false;
     }
   }
 
@@ -37,16 +74,23 @@ export class CreatesawapPage implements OnInit {
       this.showexccurr = false;
     } else {
       this.showexccurr = true;
+      this.Toamount = false;
     }
   }
   selectcurrency(list: any, index: any) {
-    this.basecurrency = list.curr
+    console.log(list);
+
+    this.fromcurrency = list.currency.code
+    this.fromwalletamount = list.wallet_amount
     this.showcurr = false;
+    this.fromamount = true;
   }
 
   selectexcurrency(list: any, index: any) {
-    this.excurrency = list.curr
+    this.excurrency = list.currency.code
+    this.towalletamount = list.wallet_amount
     this.showexccurr = false;
+    this.Toamount = true;
   }
 
 }
