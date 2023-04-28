1fa350040e42cd90099dc844a0a6df1509e0b28c
moavizq5555 <57653982+Najam-khan@users.noreply.github.com>
2023-04-28 17:30:53 +0500

    commit by najam

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -5,23 +5,17 @@ const config: CapacitorConfig = {
   appName: 'swapcircle',
   webDir: 'www',
   bundledWebRuntime: false,
-  // plugins: {
-  //   SplashScreen: {
-  //     launchShowDuration: 3000,
-  //     launchAutoHide: true,
-  //     backgroundColor: "#ffffffff",
-  //     androidSplashResourceName: "splash",
-  //     androidScaleType: "CENTER_CROP",
-  //     showSpinner: true,
-  //     androidSpinnerStyle: "large",
-  //     iosSpinnerStyle: "small",
-  //     spinnerColor: "#999999",
-  //     splashFullScreen: true,
-  //     splashImmersive: true,
-  //     layoutName: "launch_screen",
-  //     useDialog: true,
-  //   },
-  // },
+  plugins: {
+    SplashScreen: {
+      launchShowDuration: 3000,
+      launchAutoHide: true,
+      backgroundColor: "#ffffffff",
+      androidSplashResourceName: "splash",
+      showSpinner: false,
+      splashFullScreen: true,
+      splashImmersive: true,
+    },
+  },
 };
 
 export default config;
